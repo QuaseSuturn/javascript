1be2c3454a181b74c20944d2fa096fb0ceb465f2
AlmostSuturn <sethkyre@gmail.com>
2022-01-17 22:48:12 -0300

    Exercícios Módulo F

---
+++ aula16/ambiente.js
@@ -0,0 +1,7 @@
+var num = [5,8,2,9,3]
+num.sort()
+for(let pos in num){
+    console.log(num[pos])
+}
+
+console.log(`O valor 8 está na posição ${num.indexOf(8)}`)
---
+++ aula17/ambiente.js
@@ -0,0 +1,6 @@
+let v = function(x){
+    return x*2
+}
+
+v += v(5)
+console.log(v)
